397f8e9f0ae009c5df79952cb30deb55808931bc
Caleb Everett <everett.caleb@gmail.com>
2013-02-27 13:46:35 -0500

    Copied init.c and proc.c from github

--- kern/init.c
+++ kern/init.c
@@ -12,6 +12,10 @@
 #include <inc/string.h>
 #include <inc/assert.h>
 #include <inc/cdefs.h>
+#if LAB >= 3
+#include <inc/elf.h>
+#include <inc/vm.h>
+#endif
 
 #include <kern/init.h>
 #include <kern/cons.h>
@@ -19,22 +23,51 @@
 #include <kern/mem.h>
 #include <kern/cpu.h>
 #include <kern/trap.h>
+#if LAB >= 2
 #include <kern/spinlock.h>
 #include <kern/mp.h>
 #include <kern/proc.h>
+#endif
+#if LAB >= 4
+#include <kern/file.h>
+#endif	// LAB >= 4
+#if LAB >= 5
+#include <kern/net.h>
+#endif	// LAB >= 5
 
+#if LAB >= 2
 #include <dev/pic.h>
 #include <dev/lapic.h>
 #include <dev/ioapic.h>
+#if SOL >= 3	// XXX rdtsc calibration
+#include <dev/nvram.h>
+#endif
+#endif	// LAB >= 2
+#if LAB >= 5
+#include <dev/pci.h>
+#endif	// LAB >= 5
+#if SOL >= 4
+#include <dev/pmc.h>
+#endif
+#if LAB >= 9
+#include <dev/timer.h>
+#endif
 
 
 // User-mode stack for user(), below, to run on.
 static char gcc_aligned(16) user_stack[PAGESIZE];
 
-#define ROOTEXE_START _binary_obj_user_sh_start
-
 // Lab 3: ELF executable containing root process, linked into the kernel
 #ifndef ROOTEXE_START
+#if LAB == 3
+#define ROOTEXE_START _binary_obj_user_testvm_start
+#elif LAB == 4
+#define ROOTEXE_START _binary_obj_user_testfs_start
+#elif LAB >= 5
+#define ROOTEXE_START _binary_obj_user_sh_start
+#else
+#define ROOTEXE_START _binary_obj_user_sh_start
+#endif // LAB >= 5
 #endif
 extern char ROOTEXE_START[];
 
@@ -57,10 +90,19 @@ init(void)
 	// Can't call cprintf until after we do this!
 	cons_init();
 
+	//copy the low memory bootothers code.
+	extern uint8_t _binary_obj_boot_bootother_start[],
+			_binary_obj_boot_bootother_size[];
+	uint8_t *code = (uint8_t*)lowmem_bootother_vec;
+	memmove(code, _binary_obj_boot_bootother_start,
+		(uint32_t)_binary_obj_boot_bootother_size);
+
+#if LAB == 1
 	// Lab 1: test cprintf and debug_trace
 	cprintf("1234 decimal is %o octal!\n", 1234);
 	debug_check();
 
+#endif	// LAB == 1
 	// Initialize and load the bootstrap CPU's GDT, TSS, and IDT.
 	cpu_init();
 	trap_init();
@@ -69,42 +111,169 @@ init(void)
 	// Can't call mem_alloc until after we do this!
 	mem_init();
 
+#if LAB >= 2
 	// Lab 2: check spinlock implementation
 	if (cpu_onboot())
 		spinlock_check();
 
+#if LAB >= 3
+	// Initialize the paged virtual memory system.
+	pmap_init();
+
+#endif
 	// Find and start other processors in a multiprocessor system
 	mp_init();		// Find info about processors in system
 	pic_init();		// setup the legacy PIC (mainly to disable it)
+#if LAB >= 9
+	timer_init();		// 8253 timer, used to calibrate LAPIC timers
+#endif
 	ioapic_init();		// prepare to handle external device interrupts
 	lapic_init();		// setup this CPU's local APIC
 	cpu_bootothers();	// Get other processors started
 //	cprintf("CPU %d (%s) has booted\n", cpu_cur()->id,
 //		cpu_onboot() ? "BP" : "AP");
 
+#if LAB >= 4
+	// Initialize the I/O system.
+	file_init();		// Create root directory and console I/O files
+#if LAB >= 5
+	pci_init();		// Initialize the PCI bus and network card
+	net_init();		
+#endif // LAB >= 5
+
+#if SOL >= 4
+	cons_intenable();	// Let the console start producing interrupts
+#if LAB >= 9
+	pmc_init();		// Init perf monitoring counters
+#endif
+#else
+	// Lab 4: uncomment this when you can handle IRQ_SERIAL and IRQ_KBD.
+	//cons_intenable();	// Let the console start producing interrupts
+#endif
+
+#endif // LAB >= 4
 	// Initialize the process management code.
 	proc_init();
+#endif
 
+#if SOL == 1
+	// Conjure up a trapframe and "return" to it to enter user mode.
+	static trapframe utf = {
+#if LAB >= 9
+		gs: CPU_GDT_UDTLS | 3,
+#else
+		gs: 0,
+#endif
+		fs: 0,
+		ds: CPU_GDT_UDATA | 3,
+		es: CPU_GDT_UDATA | 3,
+		eip: (uint32_t) user,
+		cs: CPU_GDT_UCODE | 3,
+		eflags: FL_IOPL_3,	// let user() output to console
+		esp: (uint32_t) &user_stack[PAGESIZE],
+		ss: CPU_GDT_UDATA | 3,
+	};
+	trap_return(&utf);
+#elif SOL == 2
+	if (!cpu_onboot())
+		proc_sched();	// just jump right into the scheduler
+
+	// Create our first actual user-mode process
+	// (though it's still be sharing the kernel's address space for now).
+	proc *root = proc_alloc(NULL, 0);
+	root->sv.tf.eip = (uint32_t) user;
+	root->sv.tf.esp = (uint32_t) &user_stack[PAGESIZE];
+
+	proc_ready(root);	// make it ready
+	proc_sched();		// run it
+#elif SOL >= 3
+	if (!cpu_onboot())
+		proc_sched();	// just jump right into the scheduler
+
+#if LAB >= 99
+	cprintf("Calibrating timer...\n");
+	while (nvram_read(0x0a) & 0x80); // wait until no update in progress
+	uint8_t s = nvram_read(0x00), s2;
+	do {
+		while (nvram_read(0x0a) & 0x80);
+	} while ((s2 = nvram_read(0x00)) == s);
+	s = s2;
+	do {
+		while (nvram_read(0x0a) & 0x80);
+	} while ((s2 = nvram_read(0x00)) == s);
+	s = s2;
+	while (1) {
+		uint64_t ts = rdtsc();
+		do {
+			while (nvram_read(0x0a) & 0x80);
+		} while ((s2 = nvram_read(0x00)) == s);
+		s = s2;
+		uint64_t td = rdtsc() - ts;
+		cprintf("  %lld CPU clocks per second\n", td);
+	}
+#endif
+
+	// Create our first actual user-mode process
+	proc *root = proc_root = proc_alloc(NULL, 0);
+
+	elfhdr *eh = (elfhdr *)ROOTEXE_START;
+	assert(eh->e_magic == ELF_MAGIC);
+
+	// Load each program segment
+	proghdr *ph = (proghdr *) ((void *) eh + eh->e_phoff);
+	proghdr *eph = ph + eh->e_phnum;
+	for (; ph < eph; ph++) {
+		if (ph->p_type != ELF_PROG_LOAD)
+			continue;
+	
+		void *fa = (void *) eh + ROUNDDOWN(ph->p_offset, PAGESIZE);
+		uint32_t va = ROUNDDOWN(ph->p_va, PAGESIZE);
+		uint32_t zva = ph->p_va + ph->p_filesz;
+		uint32_t eva = ROUNDUP(ph->p_va + ph->p_memsz, PAGESIZE);
+
+		uint32_t perm = SYS_READ | PTE_P | PTE_U;
+		if (ph->p_flags & ELF_PROG_FLAG_WRITE)
+			perm |= SYS_WRITE | PTE_W;
+
+		for(; va < eva; va += PAGESIZE, fa += PAGESIZE) {
+			pageinfo *pi = mem_alloc(); assert(pi != NULL);
+			if (va < ROUNDDOWN(zva, PAGESIZE)) // complete page
+				memmove(mem_pi2ptr(pi), fa, PAGESIZE);
+			else if (va < zva && ph->p_filesz) {	// partial
+				memset(mem_pi2ptr(pi), 0, PAGESIZE);
+				memmove(mem_pi2ptr(pi), fa, zva-va);
+			} else			// all-zero page
+				memset(mem_pi2ptr(pi), 0, PAGESIZE);
+			pte_t *pte = pmap_insert(root->pdir, pi, va, perm);
+			assert(pte != NULL);
+		}
+	}
+
+	// Start the process at the entry indicated in the ELF header
+	root->sv.tf.eip = eh->e_entry;
+	root->sv.tf.eflags |= FL_IF;	// enable interrupts
+
+	// Give the process a 1-page stack in high memory
+	// (the process can then increase its own stack as desired)
+	pageinfo *pi = mem_alloc(); assert(pi != NULL);
+	pte_t *pte = pmap_insert(root->pdir, pi, VM_STACKHI-PAGESIZE,
+				SYS_READ | SYS_WRITE | PTE_P | PTE_U | PTE_W);
+	assert(pte != NULL);
+	root->sv.tf.esp = VM_STACKHI;
+
+#if LAB >= 4
+	// Give the root process an initial file system.
+	file_initroot(root);
+#endif
+
+	proc_ready(root);	// make the root process ready
+	proc_sched();		// run it
+#else // SOL == 0
 	// Lab 1: change this so it enters user() in user mode,
 	// running on the user_stack declared above,
 	// instead of just calling user() directly.
-  // << HEAD
 	user();
-  
-  // static trapframe utf = {
-  //   gs: 0,
-  //   fs: 0,
-  //   ds: CPU_GDT_UDATA | 3,
-  //   es: CPU_GDT_UDATA | 3,
-  //   ss: CPU_GDT_UDATA | 3,
-  //   cs: CPU_GDT_UCODE | 3,
-  //   eip: (uint32_t) user,
-  //   eflags: FL_IOPL_3,
-  //   esp: (uint32_t) &user_stack[PAGESIZE],
-  // };
-  // trap_return(&utf);
-	// //user();
-  //>>> project1
+#endif
 }
 
 // This is the first function that gets run in user mode (ring 3).
@@ -117,11 +286,14 @@ user()
 	assert(read_esp() > (uint32_t) &user_stack[0]);
 	assert(read_esp() < (uint32_t) &user_stack[sizeof(user_stack)]);
 
-	// Check the system call and process scheduling code.
-	proc_check();
-
+#if LAB == 1
 	// Check that we're in user mode and can handle traps from there.
 	trap_check_user();
+#endif
+#if LAB == 2
+	// Check the system call and process scheduling code.
+	proc_check();
+#endif
 
 	done();
 }
--- kern/proc.c
+++ kern/proc.c
@@ -1,3 +1,4 @@
+#if LAB >= 2
 /*
  * PIOS process management.
  *
@@ -15,14 +16,29 @@
 #include <kern/trap.h>
 #include <kern/proc.h>
 #include <kern/init.h>
+#if LAB >= 4
+#include <kern/file.h>
+#endif
+#if LAB >= 5
+#include <kern/net.h>
+#endif
 
+#if LAB >= 9
+#include <dev/pmc.h>
+#endif
 
 
 proc proc_null;		// null process - just leave it initialized to 0
 
 proc *proc_root;	// root process, once it's created in init()
 
+#if SOL >= 2
+static spinlock readylock;	// Spinlock protecting ready queue
+static proc *readyhead;		// Head of ready queue
+static proc **readytail;	// Tail of ready queue
+#else
 // LAB 2: insert your scheduling data structure declarations here.
+#endif
 
 
 void
@@ -31,7 +47,12 @@ proc_init(void)
 	if (!cpu_onboot())
 		return;
 
+#if SOL >= 2
+	spinlock_init(&readylock);
+	readytail = &readyhead;
+#else
 	// your module initialization code here
+#endif
 }
 
 // Allocate and initialize a new proc as child 'cn' of parent 'p'.
@@ -49,13 +70,35 @@ proc_alloc(proc *p, uint32_t cn)
 	spinlock_init(&cp->lock);
 	cp->parent = p;
 	cp->state = PROC_STOP;
+#if LAB >= 5
+	cp->home = RRCONS(net_node, mem_phys(cp), 0);
+#endif	// LAB >= 5
 
 	// Integer register state
+#if LAB >= 9
+	cp->sv.tf.gs = CPU_GDT_UDTLS | 3;
+	cp->sv.tf.fs = 0;
+#endif
 	cp->sv.tf.ds = CPU_GDT_UDATA | 3;
 	cp->sv.tf.es = CPU_GDT_UDATA | 3;
 	cp->sv.tf.cs = CPU_GDT_UCODE | 3;
 	cp->sv.tf.ss = CPU_GDT_UDATA | 3;
+#if SOL >= 2
 
+	// Floating-point register state
+	cp->sv.fx.fcw = 0x037f;	// round-to-nearest, 80-bit prec, mask excepts
+	cp->sv.fx.mxcsr = 0x00001f80;	// all MMX exceptions masked
+#endif
+
+#if SOL >= 3
+	// Allocate a page directory for this process
+	cp->pdir = pmap_newpdir();
+	cp->rpdir = pmap_newpdir();
+	if (!cp->pdir || !cp->rpdir) {
+		if (cp->pdir) pmap_freepdir(mem_ptr2pi(cp->pdir));
+		return NULL;
+	}
+#endif	// SOL >= 3
 
 	if (p)
 		p->child[cn] = cp;
@@ -66,7 +109,18 @@ proc_alloc(proc *p, uint32_t cn)
 void
 proc_ready(proc *p)
 {
+#if SOL >= 2
+	spinlock_acquire(&readylock);
+
+	p->state = PROC_READY;
+	p->readynext = NULL;
+	*readytail = p;
+	readytail = &p->readynext;
+
+	spinlock_release(&readylock);
+#else	// SOL >= 2
 	panic("proc_ready not implemented");
+#endif	// SOL >= 2
 }
 
 // Save the current process's state before switching to another process.
@@ -79,6 +133,44 @@ proc_ready(proc *p)
 void
 proc_save(proc *p, trapframe *tf, int entry)
 {
+#if SOL >= 2
+	assert(p == proc_cur());
+
+	if (tf != &p->sv.tf)
+		p->sv.tf = *tf;		// integer register state
+	if (entry == 0)
+		p->sv.tf.eip -= 2;	// back up to replay INT instruction
+#if LAB >= 9
+
+	if (p->sv.pff & PFF_USEFPU) {	// FPU state
+		assert(sizeof(p->sv.fx) == 512);
+		asm volatile("fxsave %0" : "=m" (p->sv.fx));
+		lcr0(rcr0() | CR0_TS);	// re-disable FPU
+	}
+
+	if (p->sv.pff & PFF_ICNT) {	// Instruction counting/recovery
+		//cprintf("proc_save tf %x -> proc %x\n", tf, &p->sv.tf);
+		if (p->sv.tf.eflags & FL_TF) {	// single stepping
+			if (entry > 0)
+				p->sv.icnt++;	// executed the INT insn
+			p->sv.tf.eflags &= ~FL_TF;
+			p->sv.tf.eflags |= FL_IF;
+		} else if (p->pmcmax > 0) {	// using performance counters
+			assert(pmc_get != NULL);
+			p->sv.icnt += pmc_get(p->pmcmax);
+			if (entry == 0)
+				p->sv.icnt--;	// don't count aborted INT insn
+			p->pmcmax = 0;
+			if (p->sv.icnt > p->sv.imax)
+				panic("oops, perf ctr overshoot by %d insns\n",
+					p->sv.icnt - p->sv.imax);
+		}
+		assert(p->sv.icnt <= p->sv.imax);
+	}
+	assert(!(p->sv.tf.eflags & FL_TF));
+	assert(p->pmcmax == 0);
+#endif	// LAB >= 9
+#endif	// SOL >= 2
 }
 
 // Go to sleep waiting for a given child process to finish running.
@@ -87,20 +179,127 @@ proc_save(proc *p, trapframe *tf, int entry)
 void gcc_noreturn
 proc_wait(proc *p, proc *cp, trapframe *tf)
 {
+#if SOL >= 2
+	assert(spinlock_holding(&p->lock));
+	assert(cp && cp != &proc_null);	// null proc is always stopped
+	assert(cp->state != PROC_STOP);
+
+	p->state = PROC_WAIT;
+	p->runcpu = NULL;
+	p->waitchild = cp;	// remember what child we're waiting on
+	proc_save(p, tf, 0);	// save process state before INT instruction
+
+	spinlock_release(&p->lock);
+
+	proc_sched();
+#else	// SOL >= 2
 	panic("proc_wait not implemented");
+#endif	// SOL >= 2
 }
 
 void gcc_noreturn
 proc_sched(void)
 {
+#if SOL >= 2
+	// Spin until something appears on the ready list.
+	// Would be better to use the hlt instruction and really go idle,
+	// but then we'd have to deal with inter-processor interrupts (IPIs).
+	cpu *c = cpu_cur();
+	spinlock_acquire(&readylock);
+	while (!readyhead || cpu_disabled(c)) {
+		spinlock_release(&readylock);
+
+		//cprintf("cpu %d waiting for work\n", cpu_cur()->id);
+		while (!readyhead || cpu_disabled(c)) {	// spin-wait for work
+			sti();		// enable device interrupts briefly
+			pause();	// let CPU know we're in a spin loop
+			cli();		// disable interrupts again
+		}
+		//cprintf("cpu %d found work\n", cpu_cur()->id);
+
+		spinlock_acquire(&readylock);
+		// now must recheck readyhead while holding readylock!
+	}
+
+	// Remove the next proc from the ready queue
+	proc *p = readyhead;
+	readyhead = p->readynext;
+	if (readytail == &p->readynext) {
+		assert(readyhead == NULL);	// ready queue going empty
+		readytail = &readyhead;
+	}
+	p->readynext = NULL;
+
+	spinlock_acquire(&p->lock);
+	spinlock_release(&readylock);
+
+	proc_run(p);
+
+#else	// SOL >= 2
 	panic("proc_sched not implemented");
+#endif	// SOL >= 2
 }
 
 // Switch to and run a specified process, which must already be locked.
 void gcc_noreturn
 proc_run(proc *p)
 {
+#if SOL >= 2
+	assert(spinlock_holding(&p->lock));
+
+	// Put it in running state
+	cpu *c = cpu_cur();
+	p->state = PROC_RUN;
+	p->runcpu = c;
+	c->proc = p;
+
+	spinlock_release(&p->lock);
+
+#if LAB >= 9
+	if (p->sv.pff & PFF_USEFPU) {	// FPU state
+		assert(sizeof(p->sv.fx) == 512);
+		lcr0(rcr0() & ~CR0_TS);	// enable FPU
+		asm volatile("fxrstor %0" : : "m" (p->sv.fx));
+	}
+
+	assert(!(p->sv.tf.eflags & FL_TF));
+	assert(p->pmcmax == 0);
+	if (p->sv.pff & PFF_ICNT) {	// Instruction counting/recovery
+		//cprintf("proc_run proc %x\n", &p->sv.tf);
+		if (p->sv.icnt >= p->sv.imax) {
+			warn("proc_run: icnt expired");
+			p->sv.tf.trapno = T_ICNT;
+			proc_ret(&p->sv.tf, -1);	// can't run any insns!
+		}
+		assert(p->pmcmax == 0);
+		int32_t pmax = p->sv.imax - p->sv.icnt - pmc_safety;
+		if (pmc_set != NULL && pmax > 0) {
+			assert(p->sv.tf.eflags & FL_IF);
+			assert(!(p->sv.tf.eflags & FL_TF));
+			pmc_set(pmax);
+			p->pmcmax = pmax;
+		} else {
+			p->sv.tf.eflags |= FL_TF;	// just single-step
+			// XXX taking hardware interrupts while tracing
+			// messes up our ability to count properly.
+			// Ideally we should poll for pending interrupts
+			// after each instruction we trace.
+			p->sv.tf.eflags &= ~FL_IF;
+		}
+	} else {
+		assert(!(p->sv.tf.eflags & FL_TF));
+		assert(p->pmcmax == 0);
+	}
+#endif	// LAB >= 9
+#if SOL >= 3
+	// Switch to the new process's address space.
+	lcr3(mem_phys(p->pdir));
+
+#endif
+	trap_return(&p->sv.tf);
+#else	// SOL >= 2
 	panic("proc_run not implemented");
+#endif	// SOL >= 2
 }
 
 // Yield the current CPU to another ready process.
@@ -108,7 +307,17 @@ proc_run(proc *p)
 void gcc_noreturn
 proc_yield(trapframe *tf)
 {
+#if SOL >= 2
+	proc *p = proc_cur();
+	assert(p->runcpu == cpu_cur());
+	p->runcpu = NULL;	// this process no longer running
+	proc_save(p, tf, -1);	// save this process's state
+	proc_ready(p);		// put it on tail of ready queue
+
+	proc_sched();		// schedule a process from head of ready queue
+#else
 	panic("proc_yield not implemented");
+#endif	// SOL >= 2
 }
 
 // Put the current process to sleep by "returning" to its parent process.
@@ -118,7 +327,52 @@ proc_yield(trapframe *tf)
 void gcc_noreturn
 proc_ret(trapframe *tf, int entry)
 {
+#if SOL >= 2
+	proc *cp = proc_cur();		// we're the child
+	assert(cp->state == PROC_RUN && cp->runcpu == cpu_cur());
+
+#if SOL >= 5
+	// First migrate to our home node if we're not already there.
+	if (net_node != RRNODE(cp->home)) {
+		if (entry > 0) entry = 0;	// abort syscall
+		net_migrate(tf, RRNODE(cp->home), entry);
+	}
+
+#endif
+	proc *p = cp->parent;		// find our parent
+	if (p == NULL) {		// "return" from root process!
+		if (tf->trapno != T_SYSCALL) {
+			trap_print(tf);
+			panic("trap in root process");
+		}
+#if LAB >= 4
+		// Allow the root process to do I/O via its special files.
+		// May put the root process to sleep waiting for input.
+		assert(entry == 1);
+		file_io(tf);
+#else	// LAB < 4
+		cprintf("root process terminated\n");
+		done();
+#endif	// LAB < 4
+	}
+
+	spinlock_acquire(&p->lock);	// lock both in proper order
+
+	cp->state = PROC_STOP;		// we're becoming stopped
+	cp->runcpu = NULL;		// no longer running
+	proc_save(cp, tf, entry);	// save process state after INT insn
+
+	// If parent is waiting to sync with us, wake it up.
+	if (p->state == PROC_WAIT && p->waitchild == cp) {
+		p->waitchild = NULL;
+		proc_run(p);
+	}
+
+	spinlock_release(&p->lock);
+	proc_sched();			// find and run someone else
+#else
 	panic("proc_ret not implemented");
+#endif	// SOL >= 2
 }
 
 // Helper functions for proc_check()
@@ -242,3 +496,4 @@ static void grandchild(int n)
 	panic("grandchild(): shouldn't have gotten here");
 }
 
+#endif // LAB >= 2
